56a06916342d51c50b84f007f510d0083e11fb3b
akshay <nakshay07@gmail.com>
2019-04-30 21:55:35 +0530

    first commit

---
+++ src/test/java/com/mycompany/app/LoginTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void login() {
+		System.out.println("Successfully Login Test 2");
+		
+	}
+	@ Test
+	 public void hsh() {
+		System.out.println("dhdh");
+	}
+
+}
---
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void OpeningBrowser() {
+		System.out.println("Browser opened Test 1");
+	}
+	@ Test 
+	public void hd() {
+		System.out.println("jshh");
+	}
+
+}
